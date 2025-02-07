4143daa59b16e1fa14e4ceab9ba116b72b123339
Omar <omarelopez8@gmail.com>
2025-02-06 22:28:56 -0500

    Updated db.js

--- simple-blog-backend/db.js
+++ simple-blog-backend/db.js
@@ -2,7 +2,7 @@ const { Pool } = require('pg');
 
 const pool = new Pool({
     user: 'postgres',         // Replace with your PostgreSQL username
-    host: 'localhost',        // Host of the database
+    host: 'simple-blog.chq0uccsu4k7.us-east-2.rds.amazonaws.com',        // Host of the database
     database: 'simple_blog',  // Replace with your database name
     password: 'lopez321',     // Replace with your PostgreSQL password
     port: 5432,               // Default PostgreSQL port
